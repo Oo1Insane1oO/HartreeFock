9afd1ac3783e5949a3c806961cb580d55602238a
Alocias <alocias0@gmail.com>
2018-02-15 17:42:26 +0100

    implemented overlap elements, missing kinetic and potential (and twobody)

--- src/integrals/gaussianstypeintegrals.cpp
+++ src/integrals/gaussianstypeintegrals.cpp
@@ -1,8 +1,10 @@
 #include "gaussianstypeintegrals.h"
+#include <iostream>
 
 GaussianStypeIntegrals::GaussianStypeIntegrals(const unsigned int dim, unsigned
         int numParameters, double scaling) : StypeBasis(dim) {
     m_dim = dim;
+    sqrt2pi = sqrt(2*M_PI);
 }// end constructor
 
 GaussianStypeIntegrals::~GaussianStypeIntegrals() {
@@ -17,17 +19,46 @@ void GaussianStypeIntegrals::initializeParameters(const Eigen::VectorXd&
         scalingVector) {
     /* initialize scaling factors and primitives for isotropic case */
     StypeBasis::setPrimitives(scalingVector);
+    setNormalizations();
 } // end function initializeParameters
 
 void GaussianStypeIntegrals::initializeParameters(const Eigen::MatrixXd&
         scalingMatrix) {
     /* initialize scaling factors and primitives for non-isotropic case */
     StypeBasis::setPrimitives(scalingMatrix);
+    setNormalizations();
 } // end function initializeParameters
+
+void GaussianStypeIntegrals::setNormalizations() {
+    /* calculate and set normalization factors for all basis functions */
+    normalizationFactors =
+        Eigen::ArrayXd::Zero(StypeBasis::getBasis()->getNumPrimitives());
+    for (unsigned int i = 0; i < StypeBasis::getBasis()->getNumPrimitives();
+            ++i) {
+        double prod = 1.0;
+        for (unsigned int d = 0; d < m_dim; ++d) {
+            prod *= overlapd(i,d) * overlapd(i,d);
+        } // end ford
+        normalizationFactors(i) = 1.0 / prod;
+    } // end fori
+    normalizationFactors = normalizationFactors.cwiseSqrt();
+} // end function setNormalizations
+
+double GaussianStypeIntegrals::overlapd(const unsigned int& i, const unsigned
+        int& d) {
+    /* calculate and return the 1D overlap element for state i */
+    return sqrt2pi / sqrt(StypeBasis::getBasis()->getPrimitive(i)->scaling(d));
+} // end function overlapd
         
 double GaussianStypeIntegrals::overlapElement(const unsigned int& i, const
         unsigned int& j) {
-    return 0.0;
+    double prod = 1.0;
+    for (unsigned int d = 0; d < m_dim; ++d) {
+        prod *= sqrt2pi /
+            sqrt(StypeBasis::getBasis()->getPrimitive(i)->scaling(d) +
+                    StypeBasis::getBasis()->getPrimitive(j)->scaling(d));
+    } // end ford
+    return prod * normalizationFactors(i) * normalizationFactors(j);
 } // end function overlapElement
 
 double GaussianStypeIntegrals::kineticElement(const unsigned int& i, const
